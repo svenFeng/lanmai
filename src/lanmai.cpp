05ee4d6200e46c479de399b8f57588bfa0b8c01d
svenfeng <svenfeng.hs@gmail.com>
2019-12-05 15:34:23 +0800

    rewrite log

--- src/lanmai.cpp
+++ src/lanmai.cpp
@@ -17,12 +17,25 @@
 static int8_t global_log_level = 0;
 enum LogLevel : int8_t { LL_ERROR = 0, LL_INFO = 1, LL_DEBUG = 2 };
 
-template <typename... T> void log(int8_t log_level, T... args) {
-    if (log_level <= global_log_level) {
-        printf(args...);
-        printf("\n");
+std::string ll_name(LogLevel log_level) {
+    switch (log_level) {
+        case LL_ERROR:
+            return "ERROR";
+        case LL_INFO:
+            return "INFO";
+        case LL_DEBUG:
+            return "DEBUG";
     }
 }
+
+#define log(log_level, fmt, args...)                                                                  \
+    do {                                                                                              \
+        if (log_level <= global_log_level) {                                                          \
+            printf("[%s:%d, %s][%s] ", __FILE__, __LINE__, __FUNCTION__, ll_name(log_level).c_str()); \
+            printf(fmt, ##args);                                                                      \
+            printf("\n");                                                                             \
+        }                                                                                             \
+    } while (false)
 
 int map(int code) {
     switch (code) {
